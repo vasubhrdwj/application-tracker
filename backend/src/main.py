13e2d6b16bd8b9efe356b0842f48353daf43c62e
vasubhrdwj <vasubhrdwj@gmail.com>
2025-09-05 23:48:10 +0530

    Added a health-check endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from src.api.main import api_router
+from starlette import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/health-check")
+async def health(req: Request):
+    return req.headers
 
 
 app.include_router(api_router)
