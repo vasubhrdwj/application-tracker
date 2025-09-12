4fa9e4a1b2d6a852b8585036fde95aa7a71aefd0
vasubhrdwj <vasubhrdwj@gmail.com>
2025-09-13 00:30:42 +0530

    Added User schema base and createUser schema

--- backend/src/api/routes/app_logs.py
+++ backend/src/api/routes/app_logs.py
@@ -1,17 +1,22 @@
 import json
 from fastapi import APIRouter, Depends
 from src.core.db import get_db
-from src.common.schemas import Application
+from src.common.schemas import Application, UserCreate
 from src.common.models import Application as ApplicationModel
 
 router = APIRouter(prefix="/applications", tags=["applications"])
 
 
 @router.post("", response_model=Application)
-async def create_application(app_data: Application, db = Depends(get_db)):
+async def create_application(app_data: Application, db = Depends(get_db)) -> Application:
     data = app_data.model_dump()
     db_app = ApplicationModel(**data)  
+    
     db.add(db_app)
     db.commit()
-    # db.refresh(data)
+
     return app_data
+
+@router.post("/user")
+async def create_user(data: UserCreate):
+    return data
--- backend/src/common/schemas.py
+++ backend/src/common/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import date
+from pydantic import EmailStr, Field
 
+#Application Schema
 class Application(BaseModel):
     id: int
     user_name: str
@@ -9,3 +11,14 @@ class Application(BaseModel):
     status: str = "applied"  # default status
     date_applied: date = date.today()
     notes: str | None = None
+
+
+#User Schema
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str | None = None
+    is_active: bool = True
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+
